cbbe7b6de349f494f3ada39c51184607d058ea39
chanintornchalermsuk <chanintornchalermsuk@gmail.com>
2026-06-21 11:43:23 +0700

    Complete2: เพิ่มชื่อเจ้าของโปรเจ็กต์

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Page() {
   return (
     <div>
       Hello....Main Page
+      <br />
+      ID: 6852D10099
+      <br />
+      Name: Chanintorn Chalermsuk
     </div>
   );
 }
